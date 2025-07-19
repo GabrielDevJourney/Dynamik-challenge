834ce7d344b03db29a7f750d65ee16c1eec3fe9e
Gabriel Pereira <gabspereira178@gmail.com>
2025-07-19 23:14:26 +0100

    feat: configure development tools and containerization

--- frontend/dynamikChallenge/vite.config.ts
+++ frontend/dynamikChallenge/vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        [
-          'babel-plugin-styled-components',
-          {
-            displayName: true,
-            fileName: false
-          }
-        ]
-      ]
-    }
-  })],
-  server: {
-    port: 3000,
-  },
-})
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    [
+                        'babel-plugin-styled-components',
+                        {
+                            displayName: true,
+                            fileName: false,
+                        },
+                    ],
+                ],
+            },
+        }),
+    ],
+    server: {
+        host: '0.0.0.0',
+        port: 3000,
+        watch: {
+            usePolling: true,
+        },
+    },
+});
